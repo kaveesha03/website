16deb4bd92204b867fd67658b2c01d0ab8254164
Kavithashree <73383454+kaveesha03@users.noreply.github.com>
2023-08-14 20:05:30 +0530

    Delete blog directory

--- blog/blog_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.signup, name='signup'),
-    path('h', views.home, name='home'),
-    path('c', views.create_post, name='create_post'),
-    path('d/<int:post_id>', views.delete_post, name='delete_post'),
-
-]
